2fe032f65298b44ae0ed4c34be271af9b6b570e7
PhilippeR26 <philippe.rostan@yahoo.fr>
2026-01-15 11:53:55 +0100

    fix: logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { Center, Text } from '@chakra-ui/react';
 import starknetJsImg from "../public/Images/StarkNet-JS_logo.png";
-import argentImg from "../public/Images/ArgentLogo.png";
+import readyImg from "../public/Images/ReadyLogo.png";
 import starknetReactImg from "../public/Images/starknet-react.png";
 import { DisplayConnected } from './components/client/DisplayConnected';
 import LowerBanner from './components/client/LowerBanner';
@@ -16,12 +16,10 @@ export default async function Page() {
                 Test Starknet session
             </p>
             <Center>
-                <Image src={starknetReactImg} alt='starknet-react' height={100} />
-                <Text px={4}></Text>
                 <Image src={starknetJsImg} alt='starknet.js' height={100} />
             </Center>
             <Center pt="3">
-                <Image src={argentImg} alt='ArgentX' height={65} />
+                <Image src={readyImg} alt='ArgentX' height={65} />
             </Center>
             <p className={styles.bgText}>
                 Please connect a Ready smart wallet, created in Starknet Testnet network
